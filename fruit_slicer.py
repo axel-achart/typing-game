30316599fc3edf917acec6d55dd91a85e0298294
Hippo <hippolyte.geslain@laplateforme.io>
2025-01-28 11:20:51 +0100

    Menu display done

--- fruit_slicer.py
+++ fruit_slicer.py
@@ -2,13 +2,32 @@ import pygame
 import json
 import os
 
-file_scores = 'scores.json'#file with each player and their scores
+file_scores = 'scores.json' #file with each player and their scores
 
+pygame.init()
+pygame.mixer.init()
+
+WIDTH, HEIGHT = 800, 800  
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+FONT = pygame.font.Font(None, 40)
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Fruit Sclicer")
+
+def text_display(text, x, y, color):
+    text_render=FONT.render(text,True,color)
+    screen.blit(text_render,(x,y))
 
 def menu_player(ready,current_player):
     clock = pygame.time.Clock()
     while True:
-        #display player choices
+        screen.fill(WHITE)
+        text_display("Welcome, Please choose your Player Name",50,50,BLACK)
+        text_display("Press 1 to choose from a known Player Name",50,100,BLACK)
+        text_display("Press 2 to create a Player Name",50,150,BLACK)
+        text_display("Press ESCAPE if you want to keep your username",50,HEIGHT-50,BLACK)
         pygame.display.flip()
 
         for event in pygame.event.get():
@@ -27,11 +46,22 @@ def menu_player(ready,current_player):
 
 def menu_player_known():
     clock = pygame.time.Clock()
-    players = list(scores_load().keys())  # Get the list of player names
+    players = list(scores_load().keys())
+    players_per_page = 10
+    current_page = 0
+    total_pages = (len(players)+players_per_page - 1) // players_per_page
     if players:
-        while True:            
-            #pagination
-            #display the players for the user to choose from
+        while True:
+            index_start = current_page * players_per_page
+            index_end = index_start + players_per_page
+            screen.fill(WHITE)
+            for i, player in enumerate(players[index_start:index_end]):
+                text_display(f"Player {i}:{player}",50, 100 +50*i,BLACK)
+            text_display("Welcome, Please choose your Player Name",50,50,BLACK)
+            text_display(f"Page{current_page+1}/{total_pages}",50,HEIGHT-200,BLACK)
+            text_display("Press N to go to next page",50,HEIGHT-150,BLACK)
+            text_display("Press P to go to previous page",50,HEIGHT-100,BLACK)
+            text_display("Press ESCAPE if you want to keep your username",50,HEIGHT-50,BLACK)
             pygame.display.flip()
 
             for event in pygame.event.get():
@@ -42,18 +72,27 @@ def menu_player_known():
                     if event.key == pygame.K_ESCAPE:
                         return
                     if pygame.K_1 <= event.key <= pygame.K_9:
-                        index = event.key - pygame.K_1
+                        index = (event.key - pygame.K_1)+index_start
                         if index < len(players):
                             return players[index]
                     if event.key == pygame.K_0 and len(players) >= 10:
-                        return players[9]
+                        return players[9+index_start]
+                    if event.key == pygame.K_p:
+                        if current_page > 0:
+                            current_page-=1
+                    if event.key == pygame.K_n:
+                        if index_end<len(players):
+                            current_page+=1                   
             clock.tick(30)
     
 def menu_player_new():
     clock = pygame.time.Clock()
     player = ''
     while True:
-        #display the player name as he types it
+        screen.fill(WHITE)
+        text_display("Add a new player",50,50,BLACK)
+        text_display("Type your username",50,100,BLACK)
+        text_display(f"{player}",50,150,BLACK)
         pygame.display.flip()
 
         for event in pygame.event.get():
@@ -76,14 +115,18 @@ def menu_player_new():
 def menu_player_add(player):
     scores = scores_load()
     if player not in scores:
-        scores[player]={"easy":[], "medium":[], "hard":[]}  # Ajoute un nouveau joueur avec des scores vides
+        scores[player]={"easy":[], "medium":[], "hard":[]}
     with open(file_scores, "w") as f:
         json.dump(scores, f, indent=4)
 
 def menu_difficulty(ready,current_difficulty):
     clock = pygame.time.Clock()
     while True:
-        #display difficulty choices
+        text_display("Please choose the difficulty",50,50,BLACK)
+        text_display("Press 1 for Easy mode",50,HEIGHT-200,BLACK)
+        text_display("Press 2 for Medium mode",50,HEIGHT-150,BLACK)
+        text_display("Press 3 for Hard mode",50,HEIGHT-100,BLACK)
+        text_display("Press ESCAPE if you want to keep your set difficulty",50,HEIGHT-50,BLACK)
         pygame.display.flip()
 
         for event in pygame.event.get():
@@ -102,11 +145,14 @@ def menu_difficulty(ready,current_difficulty):
                         return current_difficulty
         clock.tick(30)
 
-def menu_scores():
+def menu_gamemode(ready,current_gamemode):
     clock = pygame.time.Clock()
     while True:
-        #pagination
-        #for loop, to display each scores, for each difficulties, for each player
+        text_display("Please choose the gamemode",50,50,BLACK)
+        text_display("Press 1 for mode.1",50,HEIGHT-200,BLACK)
+        text_display("Press 2 for mode.2",50,HEIGHT-150,BLACK)
+        text_display("Press 3 for mode.3",50,HEIGHT-100,BLACK)
+        text_display("Press ESCAPE if you want to keep your set gamemode",50,HEIGHT-50,BLACK)
         pygame.display.flip()
 
         for event in pygame.event.get():
@@ -115,11 +161,55 @@ def menu_scores():
                 return
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
-                    index = +1
-                    #next page
+                    return 'mode.1'
                 elif event.key == pygame.K_2:
-                    index = -1
-                    #previous page
+                    return 'mode.2'
+                elif event.key == pygame.K_3:
+                    return 'mode.3'
+                if ready == 1:
+                    if event.key == pygame.K_ESCAPE:
+                        return current_gamemode
+        clock.tick(30)
+
+def menu_scores():
+    clock = pygame.time.Clock()
+    scores = scores_load()
+    players_per_page = 2
+    current_page = 0
+    total_pages = (len(scores)+players_per_page - 1) // players_per_page
+    while True:
+        text_display("Scores",50,50, BLACK)
+        index_start = current_page * players_per_page
+        index_end = index_start + players_per_page
+        players_list = list(scores.items())[index_start:index_end]
+        y_offset = 100
+        for player, scores_difficulty in players_list:
+            text_display(f"Player{player}",50,y_offset,BLACK)
+            y_offset+=50
+            for difficulte, player_scores in scores_difficulty.items():
+                text_display(f"Difficulty{difficulte}",70,y_offset,BLACK)
+                y_offset+=50
+                if player_scores:
+                    text_display(f"Highest Score :{max(player_scores)} points", 90,y_offset,BLACK)
+                else:
+                    text_display("No Score",90,y_offset,BLACK)
+        text_display(f"Page{current_page+1}/{total_pages}",50,HEIGHT-200,BLACK)
+        text_display("Press N to go to next page",50,HEIGHT-150,BLACK)
+        text_display("Press P to go to previous page",50,HEIGHT-100,BLACK)
+        text_display("Press ESCAPE if you want to keep your username",50,HEIGHT-50,BLACK)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    if current_page > 0:
+                        current_page-=1
+                if event.key == pygame.K_n:
+                    if index_end<len(scores):
+                        current_page+=1          
                 elif event.key == pygame.K_3:
                     return                
                 if event.key == pygame.K_ESCAPE:
@@ -146,14 +236,17 @@ def scores_save(player, score, difficulty):
     
 def menu_main():
     player = menu_player(0,0)
+    gamemode = menu_gamemode(0,0)
     difficulty = menu_difficulty(0,0)
     clock = pygame.time.Clock()
     while True:
-        #display the menus choices
-        #Play
-        #Scores
-        #Difficulty
-        #Player
+        text_display("Fruit Slicer",50,50,BLACK)
+        text_display("Press 1 to play",50,50,BLACK)
+        text_display("Press 2 to see the scores",50,100,BLACK)
+        text_display("Press 3 to change the difficulty",50,150,BLACK)
+        text_display("Press 4 to change the player",50,200,BLACK)
+        text_display("Press 5 to change the gamemode",50,250,BLACK)
+        text_display("Press 6 to quit",50,300,BLACK)
         pygame.display.flip()
         
         for event in pygame.event.get():
@@ -170,6 +263,8 @@ def menu_main():
                 if event.key == pygame.K_4:
                     player = menu_player(1,player)
                 if event.key == pygame.K_5:
+                    gamemode = menu_gamemode(1,gamemode)
+                if event.key == pygame.K_6:
                     pygame.quit()
                     return
         clock.tick(30)
